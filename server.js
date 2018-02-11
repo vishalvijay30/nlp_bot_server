852cbe1e7a1f5938a0d8e117cabbab83e2862bde
vishalvijay30 <sinjaybrothers@gmail.com>
2018-02-11 01:07:00 -0500

    Initial commit

---
+++ server.js
@@ -0,0 +1,25 @@
+//NPM imports
+const express = require('express');
+
+//create express app
+const app = express();
+
+//Local config
+const PORT = 3000;
+
+app.get('/', (req, res) => {
+    res.send("Welcome to the Webhook");
+});
+
+app.get('/ping', (req, res) => {
+    res.send('pong');
+});
+
+app.post('/webhook', (req, res) => {
+    res.send("reached webhook endpoint");
+    console.log(JSON.stringify('request'));
+});
+
+app.listen(PORT, () => {
+    console.log("Listening on port", PORT);
+});
